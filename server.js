202a47828c6a32cf8cb9966e2f78ebc2e36e9055
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-12-01 16:20:10 +0200

    HEroku Deployemnt

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
-const cors = require('cors');
+const cors = require("cors");
 //const  registerUser = require("./client/src/actions/authActions");
 
-const axios = require('axios');
+const axios = require("axios");
 const users = require("./routes/api/users");
 require("./config/passport-setup");
 const authRoutes = express.Router();
 const app = express();
-
 
 app.use(cors());
 app.use(passport.initialize());
@@ -28,16 +27,16 @@ const mongoPassword = process.env.MONGOPASS;
 // connect to URI
 const MongoClient = require("mongodb").MongoClient;
 const MONGO_URI =
-    "mongodb+srv://" +
-    mongoUser +
-    ":" +
-    mongoPassword +
-    "@hyperion-dev-leon-stevens-webdev-qiwgg.mongodb.net/Todo?retryWrites=true&w=majority";
+  "mongodb+srv://" +
+  mongoUser +
+  ":" +
+  mongoPassword +
+  "@hyperion-dev-leon-stevens-webdev-qiwgg.mongodb.net/Todo?retryWrites=true&w=majority";
 
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true })
-    .then(() => console.log("Mongo Connection successful"))
-    .catch(err => console.log("err"));
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("Mongo Connection successful"))
+  .catch((err) => console.log("err"));
 
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
@@ -48,62 +47,56 @@ app.use("/api/users", users);
 app.use("/api/posts/", require("./routes/api/posts"));
 app.use("/auth", authRoutes);
 
-
 // Google Authentication route
 
-authRoutes.route("/github").get(
+authRoutes.route("/github").get(passport.authenticate("github"));
 
-    passport.authenticate('github'));
-
-app.get('/auth/git/redirect',
-    passport.authenticate('github', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect("http://localhost:3000/blog");
-    });
-
-authRoutes.route("/google").get(
-    passport.authenticate("google", {
-        scope: [
-            "https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/userinfo.email",
-        ],
-    })
+app.get(
+  "/auth/git/redirect",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("http://localhost:3000/blog");
+  }
 );
 
+authRoutes.route("/google").get(
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
 
 // Authentication - Redirect
 authRoutes
-    .route("/google/redirect")
-    .get(passport.authenticate("google"), (req, res) => {
-        loggedInUser = req.user;
-        const user_name = req.user.user_name;
-        const email = req.user.email;
-        const password = req.user.password;
+  .route("/google/redirect")
+  .get(passport.authenticate("google"), (req, res) => {
+    loggedInUser = req.user;
+    const user_name = req.user.user_name;
+    const email = req.user.email;
+    const password = req.user.password;
 
-        console.log(' Logged In User : ', loggedInUser);
-        axios
-            .post("/api/users/signup", loggedInUser)
-            .then(res => {
-               
-                localStorage.setItem(
-                    "loginMessage",
-                    "Successfully registered. Login to continue"
-                );
-                res.redirect("http://localhost:3000/login");
-            })
-        
-
-        res.redirect("http://localhost:3000/blog");
+    console.log(" Logged In User : ", loggedInUser);
+    axios.post("/api/users/signup", loggedInUser).then((res) => {
+      localStorage.setItem(
+        "loginMessage",
+        "Successfully registered. Login to continue"
+      );
+      res.redirect("http://localhost:3000/login");
     });
 
+    res.redirect("http://localhost:3000/blog");
+  });
 
 if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
-   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-   });
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
+console.log("Mongo URI is : ", MONGO_URI);
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
