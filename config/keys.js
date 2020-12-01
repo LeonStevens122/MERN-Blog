fe7561eb7c301dd058d27626d646c1d320117f29
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-12-01 15:55:57 +0200

    Re Commit

---
+++ config/keys.js
@@ -0,0 +1,24 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '1022243923904-rmolknn5akihde144jp4lpr54sifeh6i.apps.googleusercontent.com',
+        clientSecret: 'NsTY5PIYwnNhhBB3lMhAzYTQ',
+        project_id: "level-3-task-9-274420",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs"
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://lstevs122:OCK68STUh6qBt18o@hyperion-dev-leon-stevens-webdev-qiwgg.mongodb.net/test?retryWrites=true&w=majority'
+        },
+        session: {
+            cookieKey: 'leontestcookiekey'
+    },
+    github: {
+        clientID: '1311ff39f7c20c74253f',
+        clientSecret: '4a7e714c0e7575bde6a1b2866eb23ef5131d60d6',
+
+
+    }
+};
